cd2f382f5e707b54e86216692b15e699fb2f383b
Phuc Hoang <phuchoang022002@gmail.com>
2022-08-07 15:22:30 +0700

    update port

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, 'build')));
 
 
-app.listen(9600, function() {
+app.listen(process.env.PORT || 9600, function() {
     console.log('Listening')
 })
